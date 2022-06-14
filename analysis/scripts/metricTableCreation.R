2df8f7da5aef15271af98e3275ec8aab3d18d66b
see24 <sarahendicott4@gmail.com>
2022-06-14 16:44:49 -0400

    update metrics table plot

--- analysis/scripts/metricTableCreation.R
+++ analysis/scripts/metricTableCreation.R
@@ -1,61 +1,65 @@
 ##### Metric table bar plots
-# load libraries
-source("davidfolder/roadsLibrarySource.R")
-# data (from "meanTable") in the runningProjections script
-overallData <- read.csv(paste0(data_path, "revelstoke/revelstoke_overall_means.csv"))
-cutoverData <- read.csv(paste0(data_path, "revelstoke/revelstoke_cutover_means.csv"))
+library(ggplot2)
+library(tidyverse)
+library(ggpubr)
 
-# organize data for ggplot (overall results)
-observed_valuesOVERALL <- filter(overallData, responseType == "observed")
-overallData_bars <- filter(overallData, sampleDensity %in% c("low sample density",
-                                                             "high sample density", 
-                                                             "centroid",
-                                                             "klementQGIS"))
-sampleType <- factor(overallData_bars$sampleDensity,
-                     levels = c("centroid",
-                                "low sample density",
-                                "high sample density",
-                                "klementQGIS"))
-# Create overall bar plot
-overallPlot <- ggplot(overallData_bars, aes(x = sampleType, response, fill = responseType))+
-  geom_col(position = "dodge", width = 0.75)+
-  geom_hline(aes(yintercept = response), 
-             observed_valuesOVERALL, color = "black", linetype = "dashed")+
-  facet_wrap(~factor(metric, levels=c("road density", "road presence", "distance to road", "forestry disturbance footprint","road disturbance footprint")), ncol = 1, scales = "free")+
-  ggtitle("Overall")+
-  theme_classic()+
-  scale_fill_manual(values = c("#D55E00", "#F0E442", "#0072B2", "#009E73"))+
-  theme(text = element_text(size = 15), axis.title.x = element_blank(), axis.title.y = element_blank(),
-        legend.position = "none")
+data_path_drvd <- "analysis/data/derived_data/"
 
-# Organize data for ggplot (cutover results)
-observed_valuesCUTOVER <- filter(cutoverData, responseTypeCutover == "observed")
-cutoverData_bars <- filter(cutoverData, sampleDensityCutover %in% c("low sample density",
-                                                                    "high sample density", 
-                                                                    "centroid",
-                                                                    "klementQGIS"))
-sampleTypeCutover <- factor(cutoverData_bars$sampleDensityCutover,
-                            levels = c("centroid",
-                                       "low sample density",
-                                       "high sample density",
-                                       "klementQGIS"))
+fig_path <- "analysis/figures/"
 
-# Create cutover bar plot
-cutoverPlot <- ggplot(cutoverData_bars, aes(sampleTypeCutover, responseCutover, 
-                                            fill = responseTypeCutover, na.rm = TRUE))+ 
-  geom_col(position = "dodge", width = 0.75)+
-  geom_hline(aes(yintercept = responseCutover), 
-             observed_valuesCUTOVER, color = "black", linetype = "dashed")+
-  facet_wrap(~factor(metricCutover, levels=c("road density", "road presence", "distance to road","forestry disturbance footprint","road disturbance footprint")), ncol = 1, scales = "free")+
-  ggtitle("Cutover")+
-  theme_classic()+
-  scale_fill_manual(values = c("#D55E00", "#F0E442", "#0072B2", "#009E73"))+
-  theme(text = element_text(size = 15), axis.title.x = element_blank(), axis.title.y = element_blank(),
-        legend.position = "none")
+fig_dev <- "jpeg"
 
-cutoverPlot
-overallPlot
-# Organize into one panel for paper
-grid.arrange(cutoverPlot, overallPlot, ncol=2)
+# data (from "meanTable") in the runProjections script
+meanTable <- read.csv(paste0(data_path_drvd, "mean_table_1e-06.csv"))
+
+# organize data for ggplot
+meanTable_long <- meanTable %>%
+  pivot_longer(-c(sampleType, sampleDens, areaMean),
+               names_to = "metric", values_to = "response")%>%
+  mutate(sampleDens = factor(sampleDens,
+                             levels = c("centroid",
+                                        "low sample density",
+                                        "high sample density",
+                                        "klementQGIS"),
+                             labels = c("Centroid", "Low\ndensity", "High\ndensity",
+                                        "Klement\nQGIS")),
+         metric = factor(metric,
+                         levels = c("roadDensityMean", "roadPresenceMean",
+                                    "distanceToRoadMean",
+                                    "forestryDisturbanceMean",
+                                    "roadDisturbanceMean"),
+                         labels = c("Road density", "Road presence",
+                                    "Distance to road",
+                                    "Forestry disturbance footprint",
+                                    "Road disturbance footprint")))
+
+
+
+observed_values <- filter(meanTable_long, sampleType == "observed")
+projected_values <- filter(meanTable_long, !is.na(sampleDens))
+# Create bar plot
+fig3 <- projected_values %>% group_by(areaMean) %>% nest() %>% ungroup() %>%
+  mutate(obs = observed_values %>% group_by(areaMean) %>% nest() %>% pull(data)) %>%
+  group_by(areaMean) %>%
+  mutate(data = map(data, ~mutate(.x, area = areaMean)),
+         plot = map2(data, obs,
+                    ~ ggplot(.x, aes(x = sampleDens, response, fill = sampleType))+
+                      geom_col(position = position_dodge2(preserve = "single",
+                                                         width = 0.75))+
+                      geom_hline(aes(yintercept = response),
+                                 .y, color = "black", linetype = "dashed")+
+                      facet_wrap(~metric, scales = "free_y", ncol = 1)+
+                      theme_classic()+
+                      scale_fill_manual(values = c("#D55E00", "#F0E442", "#0072B2", "#009E73"))+
+                      theme(text = element_text(size = 15), axis.title.x = element_blank(),
+                            axis.title.y = element_blank(),
+                            legend.position = "none")+
+                      ggtitle(str_to_title(areaMean)))) %>%
+  pull(plot) %>%
+  {ggarrange(plotlist = .)}
+
+ggsave(paste0(fig_path, "Figure3_road_metrics.", fig_dev), fig3,
+       width = 7.5, height = 9)
+
 
 
