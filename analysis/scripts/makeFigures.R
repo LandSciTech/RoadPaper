8a7552ac429d80e47ff04f55e8293010a8ea7d3f
see24 <sarahendicott4@gmail.com>
2022-06-16 13:15:07 -0400

    combine figures

--- analysis/scripts/makeFigures.R
+++ analysis/scripts/makeFigures.R
@@ -9,6 +9,9 @@ fig_path <- "analysis/figures/"
 
 fig_dev <- "jpeg"
 
+
+# Figure 3 #====================================================================
+
 # data (from "meanTable") in the runProjections script
 meanTable <- read.csv(paste0(data_path_drvd, "mean_table_1e-06.csv"))
 
@@ -61,5 +64,55 @@ fig3 <- projected_values %>% group_by(areaMean) %>% nest() %>% ungroup() %>%
 ggsave(paste0(fig_path, "Figure3_road_metrics.", fig_dev), fig3,
        width = 7.5, height = 9)
 
+# Figure 4 #====================================================================
 
+# load csv
+matchData <- read.csv(paste0(data_path_drvd, "agree_table_1e-06.csv"))
+
+matchData <- matchData %>% ungroup() %>%
+  mutate(agreement = factor(agreement,
+                            levels = c("False negative", "False positive",
+                                       "Agree roaded","Agree roadless",
+                                       "Pre-existing roads")),
+         sampleDens = paste(sampleType, sampleDens) %>%
+           str_replace("1e-06", "low density") %>%
+           str_replace("1e-04", "high density") %>%
+           str_replace("centroid low density", "centroid") %>%
+           str_replace("klementQGIS NA", "klementQGIS") %>%
+           factor(levels = c("centroid",
+                             "random low density",
+                             "random high density",
+                             "regular low density",
+                             "regular high density",
+                             "klementQGIS"),
+                  labels = c("Centroid", "Random low density", "Random high density",
+                             "Regular low density",
+                             "Regular high density",
+                             "Klement QGIS")),
+         metric = factor(metric,
+                         levels = c("roadPresence",
+                                    "forestryDisturbance",
+                                    "roadDisturbance"),
+                         labels = c("Road presence",
+                                    "Forestry disturbance footprint",
+                                    "Road disturbance footprint"))) %>%
+  group_by(metric, sampleDens, agreement) %>%
+  summarise(count = sum(count), .groups = "drop_last") %>%
+  mutate(perc = count/sum(count)*100)
+
+stackedBar <- ggplot(matchData, aes(x = sampleDens, y =perc, fill = agreement))+
+  geom_col(position = "stack", width = 0.75)+
+  theme_classic()+
+  theme(axis.text.x=element_text(color = "black", size=11, angle=50, vjust=1, hjust=1))+
+  scale_fill_manual(values = c("#D55E00", "#F0E442", "#0072B2", "#009E73", "#999999"))+
+  theme(text = element_text(size = 15), axis.title.x = element_blank())+
+  theme(legend.position="none")+
+  labs(y = "Percent of Landscape")+
+  facet_wrap(~metric)
+
+
+stackedBar
+
+ggsave(paste0(fig_path, "Figure4_agree_metrics.", fig_dev), stackedBar,
+       width = 7.5, height = 6)
 
