c17850967723c9311b86718340694d592a0f64cd
see24 <sarahendicott4@gmail.com>
2024-03-25 16:31:43 -0400

    try without Require not working from binary

--- analysis/cloud/make.R
+++ analysis/cloud/make.R
@@ -25,16 +25,21 @@ cat(print(row_ind))
 # set PAT to avoid api limit errors on GitHub
 Sys.setenv(GITHUB_PAT = "<pat>")
 
-install.packages("Require")
+install.packages("remotes")
 ## Install Dependencies (listed in DESCRIPTION) ----
 print("install deps")
-out <- try(Require::Require(c("dplyr", "sf", "terra", "tpq/peakRAM", "LandSciTech/roads")))
-cat("installed from binary:\n")
-print(out)
-
-out2 <- try(Require::Require(c("dplyr", "sf", "terra", "tpq/peakRAM", "LandSciTech/roads"),
-                             type = "source", upgrade = "never"))
-cat("installed from source?:\n")
-print(out2)
+install.packages("dplyr")
+install.packages("sf")
+install.packages("terra")
+remotes::install_github("tpq/peakRAM")
+remotes::install_github("LandSciTech/roads")
+# out <- try(Require::Require(c("dplyr", "sf", "terra", "tpq/peakRAM", "LandSciTech/roads")))
+# cat("installed from binary:\n")
+# print(out)
+#
+# out2 <- try(Require::Require(c("dplyr", "sf", "terra", "tpq/peakRAM", "LandSciTech/roads"),
+#                              type = "source", upgrade = "never"))
+# cat("installed from source?:\n")
+# print(out2)
 
 source("6_benchmark_methods.R")
